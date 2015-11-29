80f5c8f132b4fe29cfdab6a3dc06ff52084a0203
Bobby King <king.robertjoseph@gmail.com>
2015-11-29 15:32:29 -0500

    add api call to file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-alert('Loaded!');
+$(document).ready(function(){
+	// //alert("JQuery is loaded!");
+	
+	// var newH1 = $('h1').text('Added this using JQuery');
+	// var body = $('body')
+
+	// newH1.appendTo(body);
+
+	// $('.interests').click(function(e){
+	// 	console.log();
+	// });
+
+	var addToDom = function(photo){
+		//create an empty div to place photo in
+		var photoPost = $('<div></div>');
+		var myImage = $('<img src="' + photo['media']['m']+'"/>')
+		var description = $(photo['description']);
+		photoPost.append(myImage);
+		photoPost.append(description);
+		$('body').append(photoPost);
+	}
+
+	$.ajax({
+		url: 'http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?',
+		dataType: 'jsonp',
+		success: function(result){
+			var photos = result['items'];
+			console.log(result);
+			for(var i = 0; i < photos.length; i++){
+				addToDom(photos[i]);
+			}
+		}
+	});
+
+});
